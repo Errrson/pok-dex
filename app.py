ab1b81fc30fe15bfae07eeee99bbc6fad6d3f90f
Errrson <el.circuitos.jeff@gmail.com>
2023-12-02 21:30:28 +0100

    Se modifica la funcion page_pokemon

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template
-from functions.pokemon_service import get_saludo, get_pokemon_list, get_pokemon
+from classes.PokemonClass import PokemonClass
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    pokemon_list = get_pokemon_list(
-        "https://pokeapi.co/api/v2/pokemon?limit=1000")
-    return render_template("index.html", pokemon_list=pokemon_list)
+# @app.route("/")
+# def hello_world():
+#     Pokemon_list = PokemonClass.get_response(
+#         "https://pokeapi.co/api/v2/pokemon?limit=1000")
+#     return render_template("index.html", pokemon_list=Pokemon_list)
 
 
 @app.route("/pokemon/<string:nombre_pokemon>")
 def page_pokemon(nombre_pokemon):
-    pokemon_details = get_pokemon(
-        "https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon)
+    Pokemon = PokemonClass(nombre_pokemon)
+    pokemon_details = {
+        'name': Pokemon.get_name(),
+        'order': Pokemon.get_num_pokedex(),
+        'sprite': Pokemon.get_url_sprite(),
+        'sprite_shiny': Pokemon.get_url_sprite_shiny(),
+        'weight': Pokemon.get_weight(),
+        'height': Pokemon.get_height(),
+        'types': Pokemon.get_stats(),
+        'stats': Pokemon.get_stats(),
+        'evs': Pokemon.get_evolution_chain(),
+        'description': Pokemon.get_description()
+    }
 
     return render_template("pokemon_page.html", details=pokemon_details)
 
