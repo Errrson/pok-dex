5e93038031b9e073a32b1333cbfcfb35ce892d24
Errrson <el.circuitos.jeff@gmail.com>
2023-12-04 17:10:03 +0100

    Se llama a la api pokemon para poder recorrerla en el front y mostrar los pokemon en el index

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 from classes.PokemonClass import PokemonClass
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1500"
+    h = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers=h)
+    pokedex = response.json()
+    name_pokemons = pokedex['results']
+    return render_template("index.html", pokemon=name_pokemons)
 
 
 @app.route("/pokemon/<string:nombre_pokemon>")
@@ -19,7 +26,7 @@ def page_pokemon(nombre_pokemon):
         'sprite_shiny': Pokemon.get_url_sprite_shiny(),
         'weight': Pokemon.get_weight(),
         'height': Pokemon.get_height(),
-        'types': Pokemon.get_stats(),
+        'types': Pokemon.get_types(),
         'stats': Pokemon.get_stats(),
         'evs': Pokemon.get_evolution_chain(),
         'description': Pokemon.get_description()
